c23f0fcfc4b86d882530da867ce8b3e8117b99ab
asdafers <wiktor.gancarz@smykbsl.co.uk>
2018-06-29 14:45:46 +0000

    More test setup

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow } from 'enzyme';
 
-describe('<MyComponent />', () => {
+describe('App tests', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
+  it('renders welcome message', () => {
+    const wrapper = shallow(<App />);
+    const welcome = <h2>Welcome to React</h2>;
+    // expect(wrapper.contains(welcome)).to.equal(true);
+    expect(wrapper.contains(welcome)).toEqual(true);
+  });
+});
+
+describe('Component tests', () => {
   it('header has the company title', () => {
     const wrapper = shallow(
       <div>
@@ -18,12 +27,5 @@ describe('<MyComponent />', () => {
       </div>
     );
     expect(wrapper.text()).toEqual('<App />SMYK BSL');
-  });
-
-  it('renders welcome message', () => {
-    const wrapper = shallow(<App />);
-    const welcome = <h2>Welcome to React</h2>;
-    // expect(wrapper.contains(welcome)).to.equal(true);
-    expect(wrapper.contains(welcome)).toEqual(true);
   });
 });
