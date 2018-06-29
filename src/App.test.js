bd558c698af513a24f21758748f3af1b6acaee26
vagrant <vagrant@vagrant.vm>
2018-06-29 09:54:12 +0000

    Added enzyme and one broken test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, mount, render } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('has the company title', () => {
+  const wrapper = shallow(<div><App /><b>important</b></div>);
+  expect(wrapper.text()).to.equal('<App />important');
 });
