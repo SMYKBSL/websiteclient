518845d48dc6e3c2ac666f32c7c5e1abeb6659c8
vagrant <vagrant@vagrant.vm>
2018-06-29 13:36:17 +0000

    Adding more infrastructure to the app

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('<MyComponent />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 
-it('has the company title', () => {
-  const wrapper = shallow(<div><App /><b>important</b></div>);
-  expect(wrapper.text()).to.equal('<App />important');
+  it('header has the company title', () => {
+    const wrapper = shallow(
+      <div>
+        <App />
+        <h1>SMYK BSL</h1>
+      </div>
+    );
+    expect(wrapper.text()).toEqual('<App />SMYK BSL');
+  });
 });
